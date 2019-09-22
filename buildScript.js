553e988337033550ebce293007c34b39bd2cf862
Frederik Shull <frederik.shull@protonmail.com>
2019-09-22 14:02:32 +0200

    updated pull function

--- buildScript.js
+++ buildScript.js
@@ -3,9 +3,10 @@ const fse = require("fs-extra");
 const childProcess = require("child_process");
 
 if (fs.existsSync("./build")) {
-  fse.removeSync("./build");
+	fse.removeSync("./build");
 }
 
 childProcess.execSync("vue-cli-service build", { stdio: "inherit" });
 
 fse.moveSync("./build", "./server/build", { overwrite: true });
+fse.copySync("./pull.php", "./server/build/pull.php");
---
+++ pull.php
@@ -0,0 +1,1 @@
+<?php exec("cd ../../; git pull https://github.com/zlyfer/zlyfer-vebsite; npm run build"); ?>
