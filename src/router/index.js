65a473b7f04feab44c20f0fb2f52c2cfae589789
Frederik Shull <frederik.shull@protonmail.com>
2019-09-22 05:40:36 +0200

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+﻿import Vue from "vue";
+import VueRouter from "vue-router";
+
+import Grid from "@/views/Grid.vue";
+import List from "@/views/List.vue";
+import List2 from "@/views/List2.vue";
+import Blank2 from "@/views/Blank2.vue";
+Vue.use(VueRouter);
+
+// TODO Web Template Studio: Add routes for your new pages here.
+export default new VueRouter({
+  mode: "history",
+  routes: [
+    { path: "/Blank2", component: Blank2 },
+    { path: "/List2", component: List2 },
+    { path: "/List", component: List },
+    { path: "/Grid", component: Grid },
+
+    { path:"/", redirect: "/Grid" }
+  ]
+});
---
+++ src/views/Blank2.vue
@@ -0,0 +1,12 @@
+﻿<template>
+  <div/>
+</template>
+
+<script>
+export default {
+  name: "Blank2"
+};
+</script>
+
+<style scoped>
+</style>
