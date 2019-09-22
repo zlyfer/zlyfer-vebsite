24118d344ddb123849736ddd85b23797069aefe5
Frederik Shull <frederik.shull@protonmail.com>
2019-09-22 14:27:12 +0200

    test update

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -4,7 +4,13 @@
 			<div class="row justify-content-around">
 				<div class="col-8 col-md-5">
 					<h5 class="title">Welcome to zlyfer.net!</h5>
-					<p class="description">This website is still in progress.</p>
+					<p class="description">
+						This website is still in progress. Visit
+						<a
+							target="_blank"
+							href="https://github.com/zlyfer/zlyfer-vebsite"
+						>GitHub</a> for updates.
+					</p>
 				</div>
 				<div class="col-2">
 					<ul class="list-unstyled">
