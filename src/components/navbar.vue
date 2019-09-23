c9dd8a83a5e81732133e5de64f1be296890c3cc3
Frederik Shull <frederik.shull@protonmail.com>
2019-09-23 21:42:56 +0200

    removed footer

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,8 +1,5 @@
 ﻿<template>
 	<div>
-		<div class="skipLink">
-			<a href="#mainContent">Skip to Main Content</a>
-		</div>
 		<b-nav class="navbar navbar-expand-sm navbar-light border-bottom justify-content-between">
 			<a class="navbar-brand" href="/">zlyfer</a>
 			<div class="navbar-nav">
@@ -15,24 +12,9 @@
 
 <script>
 	export default {
-		name: "TheNavBar"
+		name: "navbar"
 	};
 </script>
 
 <style scoped>
-	.skipLink a {
-		position: absolute;
-		left: -100px;
-		top: -100px;
-	}
-
-	.skipLink a:focus {
-		position: fixed;
-		z-index: 1000;
-		top: 0;
-		left: 0;
-		padding: 10px;
-		color: #ffffff;
-		background: #000000;
-	}
 </style>
