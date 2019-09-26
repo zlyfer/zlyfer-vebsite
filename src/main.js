ded8a4d123cd51dfd88ff803b8c77590194081e0
Frederik Shull <frederik.shull@protonmail.com>
2019-09-26 07:06:36 +0200

    oppsie doopsie

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "@/router";
 import Vue from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.min.css";
-import "../node_modules/swagger-ui/dist/swagger-ui.css";
+// import "../node_modules/swagger-ui/dist/swagger-ui.css";
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
