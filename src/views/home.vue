e6df7a43fcbc81f24c6d2fdf0ce41aec09fd0238
Frederik Shull <frederik.shull@protonmail.com>
2020-04-21 21:40:07 +0200

    test

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,47 +1,44 @@
 <template>
-	<div id="home">
-		<b-jumbotron
-			header="zlyfer.net!!"
-			lead="Meine persönliche Website mit meinen Projekten"
-		>
-			<hr class="my-4" />
-			<p>
-				Dies ist die neuste überarbeitete Version. Von Zeit zu Zeit werden alle
-				alten bekannten Features implementiert.
-			</p>
-			<b-button variant="primary" target="_blank" href="https://old.zlyfer.net">
-				Alte Version
-			</b-button>
-		</b-jumbotron>
-	</div>
+  <div id="home">
+    <b-jumbotron header="zlyfer.net!!" lead="Meine persönliche Website mit meinen Projekten">
+      <hr class="my-4" />
+      <p>
+        Dies ist die neuste überarbeitete Version. Von Zeit zu Zeit werden alle alten bekannten Features implementiert.
+        Test
+      </p>
+      <b-button variant="primary" target="_blank" href="https://old.zlyfer.net">
+        Alte Version
+      </b-button>
+    </b-jumbotron>
+  </div>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {};
-		},
-		mounted() {}
-	};
+export default {
+  data() {
+    return {};
+  },
+  mounted() {},
+};
 </script>
 
 <style>
-	:root {
-		--accent-color: hsl(215, 90%, 60%);
-		--light-accent-color: hsl(215, 90%, 65%);
-		--dark-accent-color: hsl(215, 90%, 55%);
-		--background-color: #ffffff;
-	}
-	*::-webkit-scrollbar-track {
-		background-color: var(--background-color);
-	}
-	*::-webkit-scrollbar {
-		width: 15px;
-	}
-	*::-webkit-scrollbar-thumb {
-		background-color: var(--light-accent-color);
-	}
-	*::-webkit-scrollbar-thumb:hover {
-		background-color: var(--dark-accent-color);
-	}
+:root {
+  --accent-color: hsl(215, 90%, 60%);
+  --light-accent-color: hsl(215, 90%, 65%);
+  --dark-accent-color: hsl(215, 90%, 55%);
+  --background-color: #ffffff;
+}
+*::-webkit-scrollbar-track {
+  background-color: var(--background-color);
+}
+*::-webkit-scrollbar {
+  width: 15px;
+}
+*::-webkit-scrollbar-thumb {
+  background-color: var(--light-accent-color);
+}
+*::-webkit-scrollbar-thumb:hover {
+  background-color: var(--dark-accent-color);
+}
 </style>
